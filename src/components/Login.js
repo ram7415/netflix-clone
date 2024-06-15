3cf0edd23fddde0db07afa7064d0417f41290be4
ram7415 <yram9757@gmail.com>
2024-06-15 14:40:05 +0530

    using useref

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignForm] = useState(true);
 
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+    const message = checkValidData(email.current.value, password.current.value);
+  };
   const toggleSignInForm = () => {
     setIsSignForm(!isSignInForm);
   };
@@ -16,7 +26,10 @@ const Login = () => {
           alt="background "
         />
       </div>
-      <form className="absolute p-12 bg-black w-4/12 my-36 mx-auto right-0 left-0 text-white bg-opacity-70 rounded-lg">
+      <form
+        className="absolute p-12 py-12 bg-black w-4/12 my-36  mx-auto right-0 left-0 text-white bg-opacity-70 rounded-lg"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="text-3xl font-bold py-4  ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -28,17 +41,19 @@ const Login = () => {
           />
         )}
         <input
+          useRef={email}
           type="text"
           placeholder="Email Addresss"
           className="p-4 my-4 w-full  bg-gray-700 rounded-lg"
         />
         <input
+          useRef={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700 rounded-lg"
         />
         <button className="p-4 my-6 w-full bg-red-700 rounded-lg">
-          {isSignInForm ? "Sign In" : "Sign Up"}
+          {isSignInForm ? "Sign In" : "Sign Up"} onClick={handleButtonClick}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
           {isSignInForm
