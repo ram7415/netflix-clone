11bcb0ff6b9ce2a5ea5c8be7246219ba6d8480ae
ram7415 <yram9757@gmail.com>
2024-06-29 13:43:47 +0530

    added utom hook for api calling

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      API_OPTIONS
-    );
-
-    const json = await data.json();
-    console.log(json.results);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  });
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./userSlice";
 
+import moviesSlice from "./moviesSlice";
+
 const appStore = configureStore({
   reducer: {
     user: useReducer,
+    movies: moviesSlice,
   },
 });
 export default appStore;
