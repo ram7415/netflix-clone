7e7e4378bd0a710945624040b8492e7db6150e73
ram7415 <yram9757@gmail.com>
2024-06-29 13:43:47 +0530

    added utom hook for api calling

---
+++ src/utils/moviesSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+const moviesSlice = createSlice({
+  name: "movies",
+  initialState: {
+    nowPlayingMovies: null,
+  },
+  reducers: {
+    addNowPlayingMovies: (State, action) => {
+      State.nowPlayingMovies = action.payload;
+    },
+  },
+});
+
+export const { addNowPlayingMovies } = moviesSlice.actions;
+export default moviesSlice.reducer;
