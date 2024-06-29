f1f94e0a965a9d4979ffa67d778d161458987036
ram7415 <yram9757@gmail.com>
2024-06-29 19:34:14 +0530

    trailer added successfully

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -27,10 +27,10 @@ const VideoBackground = ({ movieId }) => {
         height="315"
         src="https://www.youtube.com/embed/L4DrolmDxmw?si=lI1gwLq-8-VLMDGt"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
