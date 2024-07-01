45ab66c10abb633e3aa9e54453cc1bf98079e4fc
ram7415 <yram9757@gmail.com>
2024-07-01 13:01:54 +0530

    added trailer id dynamic

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -2,21 +2,23 @@ import { API_OPTIONS } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
-const useMovieTrailer = () => {
+const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
   const getMoviesVideos = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1022789/videos?language=en-US",
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
       API_OPTIONS
     );
     const json = await data.json();
-    console.log("trailer", json);
+    // console.log("trailer", json);
 
     const filterData = json.results?.filter(
       (video) => video.type === "Trailer"
     );
     const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
+    // console.log(trailer);
     dispatch(addTrailerVideo(trailer));
   };
 
