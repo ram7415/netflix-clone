8385428f4e35b1702a0c1d16fb7abb61d01e3ab3
ram7415 <yram9757@gmail.com>
2024-07-03 12:02:26 +0530

    addedtrailerslice

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -4,11 +4,14 @@ const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
   return (
-    <div>
+    <div className="">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
