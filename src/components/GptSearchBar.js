878a0a77bba8142995e20e00fc1f4c563a2eede0
ram7415 <yram9757@gmail.com>
2024-07-03 22:46:58 +0530

    added openai for movie suggestion

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import openai from "../utils/openai";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -81,21 +82,52 @@ const GptSearchBar = () => {
         >
 =======
 import React from "react";
+=======
+import React, { useRef } from "react";
+>>>>>>> 501cc2d (added openai for movie suggestion)
 import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
+import openai from "../utils/openai";
 
 const GptSearchBar = () => {
+  const searchText = useRef(null);
   const langKey = useSelector((store) => store.config.lang);
+
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value);
+    //make an api call to GPt Api and movie result
+    const gptQuery =
+      "Act as a Movie Recomendation System and Suggest some movies for the query" +
+      searchText.current.value +
+      "only give me name of 5 movies,comma separated like the example result given ahead.Example Result :Gadar,Sholey,Don,Krish,Kalki";
+
+    const gptResults = await openai.chat.compeletions.create({
+      messages: [{ role: "user", content: { gptQuery } }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(gptResults.choices);
+  };
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className=" w-1/2 bg-black grid grid-cols-12">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className=" w-1/2 bg-black grid grid-cols-12"
+      >
         <input
           type="text"
+          ref={searchText}
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
+<<<<<<< HEAD
         <button className="py-2  bg-red-700 text-white rounded-lg col-span-3 px-4 m-4">
 >>>>>>> 7eef0a5 (added languages in gpt searchbox)
+=======
+        <button
+          onClick={handleGptSearchClick}
+          className="py-2  bg-red-700 text-white rounded-lg col-span-3 px-4 m-4"
+        >
+>>>>>>> 501cc2d (added openai for movie suggestion)
           {lang[langKey].search}
         </button>
       </form>
